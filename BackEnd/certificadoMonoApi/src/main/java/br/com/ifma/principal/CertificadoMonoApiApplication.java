86afcc4718349f833bd58477686deae2509c8a56
jamil2gomes <jamil.lannister23@gmail.com>
2020-02-04 13:09:21 -0300

    first commit

---
+++ BackEnd/certificadoMonoApi/src/main/java/br/com/ifma/principal/CertificadoMonoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifma.principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificadoMonoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificadoMonoApiApplication.class, args);
+	}
+
+}
---
+++ BackEnd/certificadoMonoApi/src/test/java/br/com/ifma/principal/CertificadoMonoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifma.principal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificadoMonoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
