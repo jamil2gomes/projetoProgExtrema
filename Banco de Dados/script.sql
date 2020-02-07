2f4c9db552b1913f4cc292f52cfe025179b3251c
paulohrsodre <paulohrsodre@hotmail.com>
2020-02-07 18:17:54 -0300

    desativação dos inputs (readonly)

--- Banco de Dados/script.sql
+++ Banco de Dados/script.sql
@@ -23,43 +23,43 @@ select * from monografia;
 select * from professor_banca;
 
 
-#PEGA A DATA, LOCAL, TEMA, SITUACAO E NOME DO ALUNO
-select  m.data_defesa, m.local, a.nome, m.tema, m.situacao 
-from aluno a 
+/*PEGA A DATA, LOCAL, TEMA, SITUACAO E NOME DO ALUNO*/
+select  m.data_defesa, m.local, a.nome, m.tema, m.situacao
+from aluno a
 inner join monografia m on  a. id = m.aluno_id
-where m.situacao = "APROVADO" 
+where m.situacao = "APROVADO"
 and a.nome = "Pedro Silva"
 and m.tema = "Tema X";
 
-#PEGA OS PROFESSORES DA BANCA E SUAS NOTAS
+/*PEGA OS PROFESSORES DA BANCA E SUAS NOTAS*/
 select p.nome, pb.nota from aluno a inner join monografia m  on a.id = m.aluno_id
-inner join banca b on b.monografia_id = m.id inner join professor_banca pb on pb.banca_id = b.id 
+inner join banca b on b.monografia_id = m.id inner join professor_banca pb on pb.banca_id = b.id
 inner join professor p on p.id = pb.professor_id
-where m.situacao = "APROVADO" 
+where m.situacao = "APROVADO"
 and a.nome = "Pedro Silva"
 and m.tema = "Tema X";
 
 select p.nome, pb.nota from professor p inner join professor_banca pb on p.id = pb.professor_id
 inner join banca b on b.id = pb.banca_id inner join monografia m on m.id = b.monografia_id inner join aluno a
 on a.id = m.aluno_id
-where m.situacao = "APROVADO" 
+where m.situacao = "APROVADO"
 and a.nome = "Pedro Silva"
 and m.tema = "Tema X";
 
 
-#PEGA A MEDIA DA NOTA DO ALUNO
+/*PEGA A MEDIA DA NOTA DO ALUNO*/
 select round(avg(pb.nota),2) from aluno a inner join monografia m  on a.id = m.aluno_id
-inner join banca b on b.monografia_id = m.id inner join professor_banca pb on pb.banca_id = b.id 
-where m.situacao = "APROVADO" 
+inner join banca b on b.monografia_id = m.id inner join professor_banca pb on pb.banca_id = b.id
+where m.situacao = "APROVADO"
 and a.nome = "Pedro Silva"
 and m.tema = "Tema X";
 
 
-#PEGA O NOME DA ORIENTADORA
+/*PEGA O NOME DA ORIENTADORA*/
 select p.nome from aluno a inner join monografia m  on a.id = m.aluno_id
-inner join banca b on b.monografia_id = m.id inner join professor_banca pb on pb.banca_id = b.id 
+inner join banca b on b.monografia_id = m.id inner join professor_banca pb on pb.banca_id = b.id
 inner join professor p on p.id = pb.professor_id
-where m.situacao = "APROVADO" 
+where m.situacao = "APROVADO"
 and pb.orientador = true
 and a.nome = "Pedro Silva"
 and m.tema = "Tema X";
