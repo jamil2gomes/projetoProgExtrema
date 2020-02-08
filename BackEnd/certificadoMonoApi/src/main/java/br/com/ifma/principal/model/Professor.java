17f942714cf78484e599355606d393a5aa52863a
jamil2gomes <jamil.lannister23@gmail.com>
2020-02-08 15:45:12 -0300

    melhoria no backend

--- BackEnd/certificadoMonoApi/src/main/java/br/com/ifma/principal/model/Professor.java
+++ BackEnd/certificadoMonoApi/src/main/java/br/com/ifma/principal/model/Professor.java
@@ -33,6 +33,12 @@ public class Professor implements Serializable {
 	
 	public Professor() {}
 	
+	
+	
+	public Professor(String nome) {this.nome = nome;}
+
+
+
 	public String getNome() {return nome;}
 
 	public void setNome(String nome) {this.nome = nome;}
